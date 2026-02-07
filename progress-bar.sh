f10bd73c4cc1f2623ab5f48816ce94b6807c6c76
Vinicius Costa <vinicosta.vn3dev@gmail.com>
2026-02-07 16:40:31 -0300

    add per-file processing loop

--- progress-bar.sh
+++ progress-bar.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+progress-bar() {
+    local i=$1 # primeiro argumento $((i+1))
+    local len=$2 # segundo argumeto $len
+
+    echo "processing $1/$len"
+}
+
 # ativa globstar para busca recursiva (**)
 # nullglob para se n encontrar nada, criar um array vazio
 shopt -s globstar nullglob
@@ -8,5 +15,12 @@ echo "finding files"
 files=(./**/*cache)
 # armazena a quantidade de arquivos encontrados
 len=${#files[@]}
-
 echo "found $len files"
+
+i=0
+for file in "${files[@]}"; do
+    # chama function passando os 2 argumentos
+    progress-bar "$((i+1))" "$len"
+
+    ((i++))
+done
